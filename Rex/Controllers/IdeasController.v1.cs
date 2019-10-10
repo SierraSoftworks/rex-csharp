38f972b1ca7d93b71155419bc8579d200ee2bd78
Benjamin Pannell <benjamin@pannell.dev>
2019-10-10 22:44:33 +0100

    fix: Remove admin only authorization for idea v1 controller

--- Rex/Controllers/IdeasController.v1.cs
+++ Rex/Controllers/IdeasController.v1.cs
@@ -9,7 +9,6 @@ namespace Rex.Controllers
 {
     [Area("v1")]
     [ApiController]
-    [Authorize(Roles = "Administrator")]
     public class IdeaV1Controller : IdeaController<Idea.Version1>
     {
         public IdeaV1Controller(IIdeaStore store, IRoleAssignmentStore roleStore, IRepresenter<Idea, Idea.Version1> representer, ILogger<IdeaController<Idea.Version1>> logger) : base(store, roleStore, representer, logger)
