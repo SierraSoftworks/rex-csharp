cd5385bae1363063b8956b17c28e5528bd404e4d
Benjamin Pannell <benjamin@pannell.dev>
2019-10-10 22:44:02 +0100

    feat: Add a controller to expose auth claims for debugging purposes

---
+++ Rex/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rex.Controllers
+{
+    [Area("v1")]
+    [Route("api/[area]/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public virtual ActionResult GetClaims() => Ok(this.User.Claims.GroupBy(c => c.Type).ToDictionary(g => g.Key, g => g.Count() == 1 ? (object)g.Single().Value : g.Select(c => c.Value).ToArray()));
+    }
+}
--- Rex/Startup.cs
+++ Rex/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -42,7 +43,7 @@ namespace Rex
                     {
                         Configuration["Authentication:AppIdUri"],
                         Configuration["Authentication:ClientId"]
-                    }
+                    },
                 };
             });
 
