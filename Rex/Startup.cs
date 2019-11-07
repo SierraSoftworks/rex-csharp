5cc9d9d250ff403d159534cea3e4882e262e2371
Benjamin Pannell <benjamin@pannell.dev>
2019-11-07 21:10:45 +0000

    tweak: Switch how allow all origins is enabled for CORS

--- Rex/Startup.cs
+++ Rex/Startup.cs
@@ -31,7 +31,7 @@ namespace Rex
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(o => o.AddDefaultPolicy(c => c.AllowAnyOrigin().AllowCredentials()));
+            services.AddCors(o => o.AddDefaultPolicy(c => c.AllowCredentials().SetIsOriginAllowed(_ => true)));
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
             {
